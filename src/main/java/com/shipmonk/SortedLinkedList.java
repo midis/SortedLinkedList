fa20eeb9443c25151192239c7c6b69c0401515a2
lubomir.peterka <lubomir.peterka@gmail.com>
2025-07-11 13:14:18 +0200

    Adding the initial implementation of methods add() and get() with the appropriate simple unit test.

--- src/main/java/com/shipmonk/SortedLinkedList.java
+++ src/main/java/com/shipmonk/SortedLinkedList.java
@@ -1,16 +1,34 @@
 package com.shipmonk;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Linked list implementation which keeps the elements always sorted.
  */
 public class SortedLinkedList {
 
-    private int size = 0;
+    private final List<Integer> list = new LinkedList<>();
 
     /**
      * @return number of elements in the list
      */
     public int size() {
-        return size;
+        return list.size();
+    }
+
+    /**
+     * @param newElement element which is to be added to the list
+     */
+    public void add(final int newElement) {
+        list.add(newElement);
+    }
+
+    /**
+     * @param index of the element in the list to be returned (starting by 0)
+     * @return element on the specified position (index) in the list
+     */
+    public int get(final int index) {
+        return list.get(index);
     }
 }
--- src/test/java/com/shipmonk/SortedLinkedListTest.java
+++ src/test/java/com/shipmonk/SortedLinkedListTest.java
@@ -2,6 +2,8 @@ package com.shipmonk;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class SortedLinkedListTest {
 
@@ -16,5 +18,14 @@ class SortedLinkedListTest {
     void newEmptyListHasZeroSize() {
         SortedLinkedList list = new SortedLinkedList();
         Assertions.assertEquals(0, list.size());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 11, Integer.MAX_VALUE, -1, -111, Integer.MIN_VALUE})
+    void addOneIntegerToEmptyListUpdatesSizeAndGetReturnsIt(int value) {
+        SortedLinkedList list = new SortedLinkedList();
+        list.add(value);
+        Assertions.assertEquals(1, list.size());
+        Assertions.assertEquals(value, list.get(0));
     }
 }
